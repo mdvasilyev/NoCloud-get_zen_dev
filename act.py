bdfc632ade3457b769c96513b77f87a9d7cb0151
Maxim Vasilyev <mayxis@inbox.ru>
2024-03-09 01:33:11 +0300

    feat: add previous month start/end vars and support CLI for custom start/end vars

--- act.py
+++ act.py
@@ -1,12 +1,13 @@
 import datetime
 from docx import Document
 from docx2pdf import convert
+import sys
 from python_docx_replace import docx_replace
 
 
 def main():
     template_path: str = "Act-template.docx"
-    monthsConvert = {
+    monthsConvert: dict = {
         "January": "января",
         "February": "февраля",
         "March": "марта",
@@ -23,11 +24,19 @@ def main():
     today = datetime.date.today()
     monthEn = today.strftime("%B")
     monthRu = monthsConvert.get(monthEn)
+    if len(sys.argv) == 1:
+        periodEnd = datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
+        periodStart = periodEnd.replace(day=1)
+        periodEnd = periodEnd.strftime("%d.%m.%Y")
+        periodStart = periodStart.strftime("%d.%m.%Y")
+    else:
+        periodStart = sys.argv[1]
+        periodEnd = sys.argv[2]
     properties: dict = {
         "TODAY-RU": today.strftime(f"«%d» {monthRu} %Y г."),
         "TODAY-EN": today.strftime("“%d” %B %Y"),
-        "PERIOD-START": "01.02.2024",
-        "PERIOD-END": "28.02.2024"
+        "PERIOD-START": f"{periodStart}",
+        "PERIOD-END": f"{periodEnd}"
     }
     doc = Document(template_path)
     docx_replace(doc, **properties)
